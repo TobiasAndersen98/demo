573d0fd6ad08098a14e29e4ac6f1b572db973c52
45275 <karl.tobias.andersen@gmail.com>
2021-02-08 15:43:18 +0100

    Føste push tilføget ecco og calc metoder og endpoint

--- src/main/java/Hallo/controllers/MyFirstcontroller.java
+++ src/main/java/Hallo/controllers/MyFirstcontroller.java
@@ -2,6 +2,7 @@ package Hallo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,5 +13,17 @@ public class MyFirstcontroller {
     @ResponseBody
     public String index(){
         return "HEJ";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello World!!";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    public String calc(@RequestParam int input){
+        return "" + input*input;
     }
 }
